dcad34542584015af884068530cd69380ed191da
TomNdex <tomndebele@hotmail.co.uk>
2021-04-01 19:58:32 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+print("hello world")
+team = ("MAN UTD", "MAN CITY", "LIVERPOOL", "CHELSEA", "ARSENAL", "TOTTENHAM", "WEST HAM", "EVERTON", "LEICESTER", "ASTON VILLA")
+for club in team:
+    print(club)
